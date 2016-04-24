d41313e3161ccc124a758872f843e06b5c663bc1
hustjs <hust859@gmail.com>
2016-04-24 21:29:06 +0800

    First commit,problem42 commited.

---
+++ src/reverseSentence_42/LeftRotateString.java
@@ -0,0 +1,19 @@
+package reverseSentence_42;
+
+import java.util.Scanner;
+
+public class LeftRotateString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		String sentence=scanner.nextLine();
+		System.out.println(LeftRotate(sentence, 3));
+	}
+	public static String LeftRotate(String str,int index) {
+		str=ReverseSentence.Reverse(str, 0, index);
+		str=ReverseSentence.Reverse(str, index, str.length()-index);
+		str=ReverseSentence.Reverse(str, 0, str.length());
+		return str;
+	}
+}
---
+++ src/reverseSentence_42/ReverseSentence.java
@@ -0,0 +1,34 @@
+package reverseSentence_42;
+
+import java.util.Scanner;
+
+public class ReverseSentence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		String sentence=scanner.nextLine();
+		System.out.println(Reverse_Sentence(sentence));
+	}
+	public static String Reverse_Sentence(String str){
+		String[] string=Reverse(str,0,str.length()).split(" ");
+		String strRS="";
+		for(int i=0;i<string.length;i++){
+			strRS=strRS+Reverse(string[i],0,string[i].length())+" ";
+		}
+		return strRS;
+	}
+	public static String Reverse(String str,int index,int length){
+		//StringBuffer strbuf=new StringBuffer(str);
+		//str=strbuf.reverse().toString();
+		char[] ch=str.toCharArray();
+		str="";
+		for(int i=0;i<index;i++)
+			str+=ch[i];
+		for(int i=length-1+index;i>index-1;i--)
+			str+=ch[i];
+		for(int i=length+index;i<ch.length;i++)
+			str+=ch[i];
+		return str;
+	}
+}
